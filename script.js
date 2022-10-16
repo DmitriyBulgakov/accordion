a1c2f3442d8f34473899b63606ddba63fb5fb60a
Bulgakov Dmitriy <dmitriybulgakov@gmail.com>
2022-10-16 12:48:56 +0200

    accordion

---
+++ script.js
@@ -0,0 +1,11 @@
+const accordion = document.getElementsByClassName('content-box');
+
+for(i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener('click', function() {
+        this.classList.toggle('active')
+        console.log(this)
+    })
+}
+
+
+console.log(accordion)
